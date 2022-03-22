4bb6258c03b69b1cb0006091ebb4643fd7d5c6a1
AYAN98542 <79016495+AYAN98542@users.noreply.github.com>
2022-03-22 17:05:43 +0530

    Update and rename aosp_lavender.mk to havoc_lavender.mk

--- havoc_lavender.mk
+++ havoc_lavender.mk
@@ -10,20 +10,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common PixelExtended stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk) 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_STOCK_ACORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-PEX_BUILD_TYPE := OFFICIAL
-PEX_MAINTAINER := Prashant
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := havoc_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -37,6 +35,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT :="google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
 
-# Pixel Extended Device Maintainers
-PRODUCT_PROPERTY_OVERRIDES += \
-    org.pex.build_maintainer=Prashant
+HAVOC_BUILD_TYPE := Unofficial
+HAVOC_MAINTAINER := Λｙａｎ (@not_ayan)
+TARGET_FACE_UNLOCK_SUPPORTED := true
