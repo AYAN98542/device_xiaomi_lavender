da3f977e15663ec40a6a8fd2f4e3bce2e08f1525
Ayan <79016495+AYAN98542@users.noreply.github.com>
2022-03-27 23:46:44 +0530

    Bringups

--- havoc_lavender.mk
+++ havoc_lavender.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common PixelExtended stuff
+# Inherit some common Havoc os stuff
 $(call inherit-product, vendor/havoc/config/common_full_phone.mk) 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
